b48be3fd38004b673a8d46f2f7ef5167a2041562
Ruslan Chufryda <rulya987654321@gmail.com>
2019-05-28 16:51:40 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String java;
+	Scanner inputDevice = new Scanner(System.in);
+	System.out.print("Введіть слово >> ");
+	java = inputDevice.next();
+	char j = java.charAt(0);
+	System.out.println("     J\n     J\n     J\nJ    J\nJJJJJJ\n" );
+	char a = java.charAt(1);
+	System.out.println("    A    \n   A A   \n  A   A  \n AAAAAAA \nA       A\n");
+	char v = java.charAt(2);
+	System.out.println("V       V\n V     V \n  V   V  \n   V V   \n    V     \n");
+	char a2 = java.charAt(3);
+	System.out.println("    A    \n   A A   \n  A   A  \n AAAAAAA \nA       A\n");
+
+	
+    }}
+
